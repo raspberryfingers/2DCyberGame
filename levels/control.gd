61bb6504bd41781536199511097dd1c7cfb29d65
Noah Gillam <30071433@student.toiohomai.ac.nz>
2024-05-17 10:34:11 +1200

    Intial Commit

---
+++ levels/control.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_host_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_start_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_join_button_pressed():
+	pass # Replace with function body.
