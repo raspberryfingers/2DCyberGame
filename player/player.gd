f562c62b6b01896099558f555c24570d5393979a
Noah Gillam <30071433@student.toiohomai.ac.nz>
2024-06-07 14:14:32 +1200

    Changed Player Code

--- player/player.gd
+++ player/player.gd
@@ -1,55 +1,96 @@
 extends CharacterBody2D
 
+enum PlayerState {
+	IDLE,
+	WALK,
+	RUN,
+	JUMP,
+	FALL,
+	CROUCH,
+	CLIMB
+}
 
-const SPEED = 170.0
-const JUMP_VELOCITY = -450.0
+var state = PlayerState.IDLE
+var facing_right = true
+var speed = 200
+var jump_power = -600
+var gravity = 2000
+var climb_speed = 100
+var is_climbing = false
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
+func _process(delta):
+	handle_input(delta)
+	update_animation()
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func handle_input(delta):
+	var motion = Vector2()
 
+	if Input.is_action_pressed("move_right"):
+		motion.x += 1
+		facing_right = true
+	elif Input.is_action_pressed("move_left"):
+		motion.x -= 1
+		facing_right = false
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if Input.is_action_just_pressed("run"):
+		state = PlayerState.RUN
+	elif Input.is_action_just_released("run"):
+		state = PlayerState.WALK
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		motion.y = jump_power
+		state = PlayerState.JUMP
 
-	# Get the input direction (input_axis) and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_axis = Input.get_axis("ui_left", "ui_right")
-	if input_axis:
-		velocity.x = input_axis * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if is_on_floor() and Input.is_action_just_pressed("crouch"):
+		state = PlayerState.CROUCH
+	elif Input.is_action_just_released("crouch"):
+		state = PlayerState.IDLE
+
+	if is_climbing:
+		if Input.is_action_pressed("climb_up"):
+			motion.y -= 1
+		elif Input.is_action_pressed("climb_down"):
+			motion.y += 1
+		else:
+			motion.y = 0
+
+	motion = motion.normalized() * speed
+	motion.y += gravity * delta  # Apply gravity based on delta time
+
+	if is_climbing:
+		motion.y = motion.y * climb_speed
 
 	move_and_slide()
-	update_animations(input_axis)
-	reset_player_position()
 
-func update_animations(input_axis):
-	
-	if input_axis > 0:
-		animated_sprite_2d.flip_h = false
-	elif input_axis < 0:
-		animated_sprite_2d.flip_h = true
-	if  is_on_floor():
-		if input_axis != 0:
-			animated_sprite_2d.play("run")
-		else:
-			animated_sprite_2d.play("idle")
+func update_animation():
+	match state:
+		PlayerState.IDLE:
+			$AnimatedSprite.play("idle")
+		PlayerState.WALK:
+			$AnimatedSprite.play("walk")
+		PlayerState.RUN:
+			$AnimatedSprite.play("run")
+		PlayerState.JUMP:
+			$AnimatedSprite.play("jump")
+		PlayerState.FALL:
+			$AnimatedSprite.play("fall")
+		PlayerState.CROUCH:
+			$AnimatedSprite.play("crouch")
+		PlayerState.CLIMB:
+			$AnimatedSprite.play("climb")
+
+	if not facing_right:
+		$AnimatedSprite.flip_h = true
 	else:
-		if velocity.y > 0:
-			animated_sprite_2d.play("fall")
-		else:
-			animated_sprite_2d.play("jump")
-	
+		$AnimatedSprite.flip_h = false
 
-func reset_player_position():
-	# used only for demonstration purposes.
-	if global_position.y > 500:
-		global_position = Vector2(100, 100)
+func _on_area_2d_body_entered(body):
+	if body is CharacterBody2D:
+		is_climbing = true
+		state = PlayerState.CLIMB # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	if body is CharacterBody2D:
+		is_climbing = false
+		state = PlayerState.IDLE # Replace with function body.
